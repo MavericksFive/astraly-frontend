8dd3f9b53dece41275a99d616709216449a8ba2d
MavericksFive <95299145+MavericksFive@users.noreply.github.com>
2022-08-25 12:44:01 +0200

    Add AutoBurn Option

--- src/components/Pages/Project/Claim/ProjectClaimPage.tsx
+++ src/components/Pages/Project/Claim/ProjectClaimPage.tsx
@@ -5,30 +5,38 @@ import ProjectLayout from '../ProjectLayout'
 import AllocationInfo from '../Main/AllocationInfo'
 import BaseButton from '../../../ui/buttons/BaseButton'
 import { useStarknetReact } from '@web3-starknet-react/core'
-import { useTokenContract, useLotteryTokenContract } from 'contracts'
+import { useTokenContract, useLotteryTokenContract, useIDOContract } from 'contracts'
+import { Result, uint256, hash } from 'starknet'
 import { ethers } from 'ethers'
-import { uint256 } from 'starknet'
 import { LockIcon, SendIcon } from '../../../ui/Icons/Icons'
 import Link from 'next/link'
 import { useTransactions } from 'context/TransactionsProvider'
 import { useQuery } from '@apollo/client'
 import { PROJECT } from '../../../../api/gql/querries'
 import Spinner from '../../../ui/Spinner/Spinner'
+import ToggleAutoBurn from 'components/ui/inputs/ToggleAutoBurn'
+import ToastActions from 'actions/toast.actions'
+import { ToastState } from 'components/ui/Toast/utils'
+import { useAppDispatch } from 'hooks/hooks'
 
 const ProjectClaimPage = () => {
   const router = useRouter()
   const { pid } = router.query
   const { account } = useStarknetReact()
   const [ticketsBalance, setTicketsBalance] = useState<string | null>(null)
+  const [userInfo, setUserInfo] = useState<Result>({} as Result)
   const [xzkpBalance, setXZkpBalance] = useState('0')
   const [project, setProject] = useState<Project | undefined>(undefined)
   const [claiming, setClaiming] = useState(false)
   const [loading, setLoading] = useState(true)
   const [hasClaimed, setHasClaimed] = useState(false)
+  const [autoBurn, setautoBurn] = useState(0)
+  const [burning, setBurning] = useState(false)
 
   const { getXZKPBalance } = useTokenContract()
-  const { claimLotteryTickets, getTicketsBalance } = useLotteryTokenContract()
-
+  const { claimLotteryTickets, getTicketsBalance, setApprovalForAll, isApprovedForAll } = useLotteryTokenContract()
+  const { getUserInfo } = useIDOContract()
+  const dispatch = useAppDispatch()
   const { addTransaction } = useTransactions()
 
   const { data } = useQuery(PROJECT, {
@@ -40,6 +48,8 @@ const ProjectClaimPage = () => {
   useEffect(() => {
     data && setProject(data.project)
   }, [data])
+
+ 
 
   const handleClaimTickets = async () => {
     if (!project) return
@@ -58,6 +68,57 @@ const ProjectClaimPage = () => {
       setClaiming(false)
     }
   }
+
+  const moderator = '0x0163A6e08d795E9A0eaBc908d7536c70735aF95ED0474B33C64f01d8b5b6E0bc'
+
+  const autoBurnTickets = async () => {
+    if (!autoBurn) {
+      try {
+        const tx = await setApprovalForAll(moderator, 1)
+        addTransaction(
+          tx,
+          'Turn ON AutoBurn',
+          () => fetchApprovalToModerator(),
+          () => {}
+        )
+      } catch (e) {
+        console.error(e)
+      }
+    } else {
+      try {
+        const tx = await setApprovalForAll(moderator, 0)
+        addTransaction(
+          tx,
+          'Turn OFF AutoBurn',
+          () => fetchApprovalToModerator(),
+          () => {}
+        )
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+
+  const fetchApprovalToModerator = async () => {
+    if (!project) return
+    try {
+      setLoading(true)
+      const approval_state = await isApprovedForAll(account?.address, moderator)
+      const value_approval = approval_state.is_approved.words[0]
+      setautoBurn(value_approval)
+      console.log(value_approval)
+      setLoading(false)
+    } catch (e) {
+      console.error(e)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (account?.address && project) {
+      fetchApprovalToModerator()
+    }
+  }, [account, project, autoBurn])
 
   const fetchBalances = async () => {
     if (!project) return
@@ -133,6 +194,10 @@ const ProjectClaimPage = () => {
                   Lock more $ASTR
                 </BaseButton>
               </Link>
+            </div>
+            <div className="flex justify-center flex-row gap-4 mt-5">
+            <div className="title--medium ">Auto-burn</div>
+              <ToggleAutoBurn value={autoBurn} onClick={() => autoBurnTickets()}></ToggleAutoBurn>
             </div>
           </div>
         </div>
--- src/contracts/lottery.ts
+++ src/contracts/lottery.ts
@@ -14,7 +14,7 @@ export const useLotteryTokenContract = () => {
   const { getContract } = useContract()
 
   const getLotteryTokenContract = async () =>
-    getContract(Contracts[CHAIN].lottery_token, LOTTERY_TOKEN_ABI)
+    getContract('0x06ff0de06e1b21e7050ed36d6fdc0f1dcec46b7300afe540c8ae6741373cd4a0', LOTTERY_TOKEN_ABI)
 
   const claimLotteryTickets = async (id: number.BigNumberish) => {
     const contract = await getLotteryTokenContract()
@@ -64,11 +64,23 @@ export const useLotteryTokenContract = () => {
     return await contract.call('balanceOf', [address, parseInputAmountToUint256(id.toString(), 0)])
   }
 
+  const setApprovalForAll = async (operator: string | undefined, approved: number) => {
+    const contract = await getLotteryTokenContract()
+    return await contract.invoke('setApprovalForAll', [operator, approved])
+  }
+
+  const isApprovedForAll = async (account: string | undefined, operator: string | undefined) => {
+    const contract = await getLotteryTokenContract()
+    return await contract.call('isApprovedForAll', [account, operator])
+  }
+
   return {
     getLotteryTokenContract,
     claimLotteryTickets,
     burn,
     getTicketsBalance,
     burnWithQuest,
+    setApprovalForAll,
+    isApprovedForAll,
   }
 }
